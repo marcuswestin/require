a32d0286c7dd3e19cd4f6a2b5d2feaf2fdd9666c
Marcus Westin <marcus.westin@gmail.com>
2011-04-21 19:16:18 -0700

    Compile correctly no matter where executed from

--- example/compile.js
+++ example/compile.js
@@ -3,12 +3,14 @@ var sys = require('sys'),
 	compiler = require('../compiler')
 
 var file = __dirname + '/client.js',
+	basePath = __dirname,
 	code = fs.readFileSync(file).toString()
 
 compileAndPrint(0, function() {
 	compileAndPrint(1, function() {
 		compileAndPrint(2, function() {
 			compileAndPrint(3, function() {
+				console.log("Done!")
 			})
 		})
 	})
@@ -16,7 +18,7 @@ compileAndPrint(0, function() {
 
 function compileAndPrint(level, callback) {
 	console.log('Compile code at level', level)
-	compiler.compile(code, level, function(err, compiledCode) {
+	compiler.compile(code, level, basePath, function(err, compiledCode) {
 		if (err) { throw err }
 		console.log(compiledCode)
 		callback()
